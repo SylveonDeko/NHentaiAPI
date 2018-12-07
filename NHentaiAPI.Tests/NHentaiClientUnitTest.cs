fa4549d4c9e990977dd34376bf18e7b0e29a5d13
為什麼 <andy840119@gmail.com>
2018-12-07 16:11:33 +0800

    根據這個專案加上基礎的網址 : https://github.com/NHMoeDev/NHentai-android/blob/master/app/src/main/kotlin/moe/feng/nhentai/api/ApiConstants.kt

--- NHentaiAPI.Tests/NHentaiClientUnitTest.cs
+++ NHentaiAPI.Tests/NHentaiClientUnitTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace HentaiAPI.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class NHentaiClientUnitTest
     {
         [TestMethod]
         public void TestMethod1()
--- NHentaiAPI/NHentaiClient.cs
+++ NHentaiAPI/NHentaiClient.cs
@@ -10,5 +10,82 @@ namespace HentaiAPI
         const string NHENTAI_HOME = "https://nhentai.net";
 	    const string NHENTAI_I = "https://i.nhentai.net";
 	    const string NHENTAI_T = "https://t.nhentai.net";
+
+        const string NHBOOKS_UA = "NHBooks ${BuildConfig.VERSION_NAME}/Android " +
+			"Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.0.0 Mobile";
+
+        #region Data urls
+
+        protected string getSearchUrl(string content,int pageNum)
+        { 
+            return $"{NHENTAI_HOME}/api/galleries/search?" +
+				$"query={content.Replace(" ", "+")}&" +
+				"page=$pageNum";
+        }
+			
+	    protected string getBookDetailsUrl(string bookId)
+        { 
+            return $"{NHENTAI_HOME}/api/gallery/{bookId}";
+        }
+			
+	    protected string getBookRecommendUrl(string bookId)
+        { 
+            return $"{NHENTAI_HOME}/api/gallery/{bookId}/related";
+        }
+			
+	    protected string getGalleryUrl(string galleryId)
+		{ 
+            return $"{NHENTAI_I}/galleries/{galleryId}";
+        }
+
+	    protected string getThumbGalleryUrl(string galleryId)
+        { 
+            return $"{NHENTAI_T}/galleries/{galleryId}";
+        }
+			
+	    protected string getTagUrl(Tag tag,bool isPopularList,int pageNum)
+        { 
+            return $"{NHENTAI_HOME}/api/galleries/tagged?" +
+					$"tag_id={tag.id}" +
+					$"&page={pageNum}" +
+					isPopularList ? "&sort=popular" : "";
+        }
+			
+	    protected string getHomePageUrl(int pageNum)
+		{
+            return $"{NHENTAI_HOME}/api/galleries/all?page={pageNum}";
+        }
+
+        #endregion
+
+        #region Picture urls
+
+        // Picture urls
+        protected string getPictureUrl(string galleryId ,string pageNum ,string fileType)
+        { 
+            return $"{getGalleryUrl(galleryId)}/{pageNum}.{fileType}";
+        }
+			
+	    protected string getThumbPictureUrl(string galleryId ,string pageNum ,string fileType)
+        { 
+            return $"{getThumbGalleryUrl(galleryId)}/${pageNum}t.{fileType}";
+        }
+			
+	    protected string getBigCoverUrl(string galleryId)
+        {
+            return $"{getThumbGalleryUrl(galleryId)}/cover.jpg";
+        }
+			
+	    protected string getOriginPictureUrl(string galleryId ,string pageNum)
+        {
+            return getPictureUrl(galleryId, pageNum, "jpg");
+        }
+			
+	    protected string getBookThumbUrl(string galleryId ,string fileType = "jpg")
+        { 
+            return $"{getThumbGalleryUrl(galleryId)}/thumb.${fileType ?? "jpg"}";
+        }
+
+        #endregion
     }
 }
