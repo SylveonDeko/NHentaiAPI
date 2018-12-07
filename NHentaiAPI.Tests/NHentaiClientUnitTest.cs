8ae01a673849643cd6a11466ffe363d46f38e1d4
為什麼 <andy840119@gmail.com>
2018-12-07 16:50:27 +0800

    remind me to fill the testcase

--- NHentaiAPI.Tests/NHentaiClientUnitTest.cs
+++ NHentaiAPI.Tests/NHentaiClientUnitTest.cs
@@ -6,8 +6,14 @@ namespace HentaiAPI.Tests
     public class NHentaiClientUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSearchResult()
         {
+            //See : https://github.com/NHMoeDev/NHentai-android/issues/27
+
+            //With this api : 
+            //https://nhentai.net/api/galleries/search?query=school%20swimsuit%20loli%20full%20color&page=2
+
+            //And verify the result
         }
     }
 }
