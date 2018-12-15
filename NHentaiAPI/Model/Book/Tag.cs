501d45e32e89aee479814d81622f1389d9d777cc
為什麼 <andy840119@gmail.com>
2018-12-15 16:47:25 +0800

    fix code issue

--- NHentaiAPI/Model/Book/Tag.cs
+++ NHentaiAPI/Model/Book/Tag.cs
@@ -5,18 +5,18 @@ namespace NHentaiAPI.Model.Book
     public class Tag
     {
         [JsonProperty("id")]
-        public int Id{ get; set;}
+        public int Id{ get; set; }
 
         [JsonProperty("type")]
-        public string Type{ get; set;}
+        public string Type{ get; set; }
 
         [JsonProperty("name")]
-        public string Name{ get; set;}
+        public string Name{ get; set; }
 
         [JsonProperty("url")]
-        public string Url { get; set;}
+        public string Url { get; set; }
 
         [JsonProperty("count")]
-        public int Count { get; set;}
+        public int Count { get; set; }
     }
 }
